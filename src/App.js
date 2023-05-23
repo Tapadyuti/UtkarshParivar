e5d813554bba5b00d3729bf29048c27afcd2ce45
Tapadyuti <tapadyutivv@gmail.com>
2023-05-24 00:47:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-purple-100 h-full">
+      <nav className="bg-purple-300">
+        Utkarsh
+      </nav>
+    </div>
+  );
+}
+
+export default App;
