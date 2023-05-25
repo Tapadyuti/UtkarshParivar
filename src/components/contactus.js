c6f955d040b212c09fbf7071d0fe4b0bb39890b8
Tapadyuti <tapadyutivv@gmail.com>
2023-05-25 14:44:50 +0530

    updating contact us page

--- src/components/contactus.js
+++ src/components/contactus.js
@@ -6,7 +6,7 @@ const ContactUs = () => {
             <div className='col-12 col-xl-5 col-lg-5 col-md-5 col-sm-12 p-2 d-flex justify-content-center'>
                 <img src={hello_cow} alt='hello' className='w-75 h-fit-content'/>
             </div>
-            <div className='col-12 col-xl-7 col-lg-7 col-md-7  col-sm-12 p-5'>
+            <div className='col-12 col-xl-7 col-lg-7 col-md-7 col-sm-12 p-5 d-flex justify-content-center'>
                 <form>
                     <div className="form-group mb-3">
                         <label htmlFor="exampleInputEmail1">Name</label>
@@ -33,7 +33,7 @@ const ContactUs = () => {
                         <label htmlFor="exampleInputPassword1">Query/Feedback</label>
                         <textarea rows="4" cols="50" className="form-control" id="exampleInputPassword1" placeholder="Your feedback are most valued to us. Any query let us know." />
                     </div>
-                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your details with anyone else.</small>
                     <br></br>
 
                     {/* <div className="form-group form-check">
